46c24d1a2cbec5552a8654219fb4b69a113228b4
windbadboy <windbadboy@outlook.com>
2017-04-19 23:34:22 +0800

    20170419
    123

---
+++ Sites/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/1/basic.css">
+	<link rel="stylesheet" href="css/1/index.css">
+	<title>Document</title>
+</head>
+<body>
+	<div id="header">
+		<h1><a href="index.php">Multi User Comment System</a></h1>
+		<ul>
+			<li><a href="#">首页</a></li>
+			<li><a href="#">注册</a></li>
+			<li><a href="#">登录</a></li>
+			<li><a href="#">个人中心</a></li>
+			<li><a href="#">风格</a></li>
+			<li><a href="#">管理</a></li>
+			<li><a href="#">退出</a></li>
+		</ul>
+	</div>
+	<div id="list">
+		<h2>帖子列表</h2>
+	</div>
+	<div id="user">
+		<h2>新进会员</h2>
+	</div>
+	<div id="pics">
+		<h2>最新图片</h2>
+	</div>
+	<div id="footer">
+		<p>电脑版 | 移动版</p>
+		<p>本程序由Ted制作,翻版必究.</p>
+	</div>
+</body>
+</html>
